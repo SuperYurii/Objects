1b04db9e8ae969fa78abb6db448ce5f5315c21bd
Yurii <pavlenkoyura88@gmail.com>
2024-12-11 09:10:05 +0000

    Comleted my workshop

---
+++ objects.js
@@ -0,0 +1,35 @@
+const person = {
+  name: "Yurii",
+  age: 36,
+  favouriteColour: "green",
+};
+
+console.log(person.name); // Output: Yurii
+
+console.log(person.age); // Output: 36
+
+console.log(person.favouriteColour); // Output: green
+
+person.favouriteFood = "Big Tasty";
+
+console.log(person.favouriteFood);
+
+let car = {
+  make: "Ford",
+  model: "C-max",
+  colour: "red",
+};
+console.log(car);
+console.log(car.make);
+console.log(car.model);
+console.log(car.colour);
+// Couldn't make request in one line. Ask Manny why?
+
+let book = {
+  title: "Stormlight archive",
+  author: "Brandon Sanderson",
+  numberOfPages: 430,
+};
+console.log(book.title);
+console.log(book.author);
+console.log(book.numberOfPages);
